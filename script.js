85dfe533464a2ca058e8e291832baea8a2bab6dd
prashant404 <pkr214204@gmail.com>
2025-05-15 21:52:42 +0530

    strings

---
+++ script.js
@@ -0,0 +1,22 @@
+//Strings
+
+//length -- returns the length of string
+
+let str = "Hello";
+console.log(str.length);
+
+//toUpperCase --  converts string to uppercase
+console.log(str.toUpperCase());
+
+//toLowerCase -- converts string to lowercase
+
+console.log(str.toLowerCase());
+
+//indexOf
+//returns the index of the first occurrence of the specified value, or -1 if not found
+let str1 = "Hello";
+console.log(str1.indexOf("l"));
+console.log(str1.indexOf("a"));
+
+
+//slice(start,end) -- extracts part of a string from start to end(excluding end)
